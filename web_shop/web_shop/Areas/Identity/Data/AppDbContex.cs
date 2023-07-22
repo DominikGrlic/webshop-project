1b9fec09abd66ab7e754ca03df465c4465eb441e
DominikGrlic <grlic.dominik@gmail.com>
2023-07-22 23:23:31 +0200

    Dodan i konfiguriran Identity paket

---
+++ web_shop/web_shop/Areas/Identity/Data/AppDbContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using web_shop.Areas.Identity.Data;
+
+namespace web_shop.Data;
+
+public class AppDbContex : IdentityDbContext<AppUser>
+{
+    public AppDbContex(DbContextOptions<AppDbContex> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- web_shop/web_shop/Program.cs
+++ web_shop/web_shop/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using web_shop.Areas.Identity.Data;
+using web_shop.Data;
 namespace web_shop
 {
     public class Program
@@ -5,6 +9,11 @@ namespace web_shop
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("AppDbContexConnection") ?? throw new InvalidOperationException("Connection string 'AppDbContexConnection' not found.");
+
+            builder.Services.AddDbContext<AppDbContex>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContex>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -23,6 +32,7 @@ namespace web_shop
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();;
 
             app.UseAuthorization();
 
