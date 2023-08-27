585c4220ac04e72e5a19d6d3912ade7bf733cf09
DominikGrlic <grlic.dominik@gmail.com>
2023-08-27 16:11:57 +0200

    Home page kartice, search i order by
    Postavljene kartice za prozvode na home page-u
    Kreiran search tab po kojem se mogu pretrazivati proizvodi po slovima i rijecima, te dodana filtracija po amebecednom nizu, cijeni i obrnuto .

--- web_shop/web_shop/Controllers/HomeController.cs
+++ web_shop/web_shop/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using web_shop.Data;
 using web_shop.Models;
 
 namespace web_shop.Controllers
@@ -9,15 +10,33 @@ namespace web_shop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContex _dbContext;
+        // dependency injection za objekt klase AppDbContext                                                                        <-------------------
+        public HomeController(ILogger<HomeController> logger, AppDbContex dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index(string? homeSearch, int orderBy = 0)
+        {            
+            List<Product> products = _dbContext.Products.ToList();
+
+            // ako parametar "homeSearch" nije prazan, filtriraj proizvode (po naslovu)
+            if (!string.IsNullOrWhiteSpace(homeSearch))
+            {
+                return View(products.Where(a => a.Title.ToLower().Contains(homeSearch.ToLower())).ToList());
+            }
+
+            switch ((int)orderBy)
+            {
+                case 1: products = products.OrderBy(a => a.Title).ToList(); break;
+                case 2: products = products.OrderByDescending(a => a.Title).ToList(); break;
+                case 3: products = products.OrderBy(a => a.Price).ToList(); break;
+                case 4: products = products.OrderByDescending(a => a.Price).ToList(); break;
+            }
+
+            return View(products);
         }
 
         public IActionResult Privacy()
